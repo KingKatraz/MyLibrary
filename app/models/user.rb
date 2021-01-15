9be460f5f1e0854699ba785712f56ac1b8f819e1
King Katraz <halnader89@gmail.com>
2021-01-15 14:19:30 -0500

    enabled user sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    has_many :books
+    has_many :genres, :through => :books
+    has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  root "sessions#home"
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
   resources :genres
   resources :users
   resources :books
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
